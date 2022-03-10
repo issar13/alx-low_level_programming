61c68213863040623c0efe58d5956268f63c6fcc
issar13 <issa_suleiman008@outlook.com>
2022-03-10 14:42:48 +0300

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.sh>
+
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
