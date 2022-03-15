25350477b13b97c6130e6ea404a4d11457655277
issar13 <issa_suleiman008@hotmail.com>
2022-03-15 13:25:59 +0300

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
+
 /**
-* main - prints a string
-* Description: the string is "Issa"
-*
-* Return: 0
-*/
+ * main - Entry point
+ * Return:0
+ */
+
 int main(void)
 {
-	char issasulei[] = "Issasulei";
-	int i = 0;
-
-	while (issasulei[i] != '\0')
-	{
-		_putchar(issasulei[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
