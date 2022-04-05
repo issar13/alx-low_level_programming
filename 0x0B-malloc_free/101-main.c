741c5cab5d5095b83f2a0482492fba152c4d0abf
issar13 <issa_suleiman008@hotmail.com>
2022-04-05 11:11:28 +0300

    malloc

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_tab - Prints an array of string
@@ -17,16 +19,15 @@ void print_tab(char **tab)
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: 1 if an error occurred, 0 otherwise
  */
 int main(void)
 {
     char **tab;
-    printf("test\n");
-    tab = strtow("Holberton School         #cisfun");
 
+    tab = strtow("      Holberton School         #cisfun      ");
     if (tab == NULL)
     {
         printf("Failed\n");
